85a39ae2479db6f65f165de5e1dbf8a5aa398fcf
Yurii Panasuik <thoryur@gmail.com>
2024-12-15 21:15:31 +0200

    report

--- main4.py
+++ main4.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from scipy.optimize import minimize
+from sklearn.decomposition import PCA
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.preprocessing import StandardScaler
@@ -20,12 +21,12 @@ parameter_names = [
 
 # input data (X), parameters
 X = np.array([
-    [3.2739, 59.99, 59.4, 255, -0.4702, 0.0168, 0.0025, 0.0032],
-    [2.8076, 55.57, 63.9, 240, -0.4900, 0.0201, 0.0009, -0.0005],
-    [2.7847, 62.39, 61.6, 280, -0.4516, -0.0191, 0.0049, 0.0023],
-    [2.5559, 55.10, 58.8, 260, -0.4310, 0.0103, 0.0010, -0.1047],
-    [2.5063, 50.99, 55.1, 260, -0.3912, 0.0149, 0.0012, -0.0094],
-    [2.5080, 51.40, 57.6, 260, -0.4353, 0.0138, 0.0006, -0.0395]
+    [3.2739, 59.99, 59.4, 255, -0.4702, 0.0168, 0.0008, 0.0032],
+    [2.8076, 55.57, 63.9, 240, -0.4900, 0.0201, 0.0012, -0.0002],
+    [2.7847, 62.39, 61.6, 280, -0.4516, -0.0191, 0.0079, 0.0006],
+    [2.5559, 55.10, 58.8, 260, -0.4310, 0.0103, 0.0017, -0.0043],
+    [2.5063, 50.99, 55.1, 260, -0.3912, 0.0149, 0.0031, -0.0071],
+    [2.5080, 51.40, 57.6, 260, -0.4353, 0.0138, 0.0001, -0.0048]
 ])
 
 # output data names (corresponding to the columns)
@@ -64,10 +65,29 @@ print(correlation_matrix.round(2).to_string())
 scaler_X = StandardScaler()
 X_normalized = scaler_X.fit_transform(X)
 
+# Principal Component Analysis
+pca = PCA(n_components=None)
+# Sensitive to outliers, they can distort the variance.
+X_pca = pca.fit_transform(X_normalized)
+
+print("Explained variance:", pca.explained_variance_)
+print("X_PCA:", X_pca)
+
+num_components = sum(pca.explained_variance_ > 1)
+print(f"Number of components selected by Kaiser Criterion: {num_components}")
+
+explained_variance_ratio = pca.explained_variance_ratio_
+cumulative_variance_ratio = np.cumsum(explained_variance_ratio)
+
+print("Explained Variance Ratio (individual components):", explained_variance_ratio)
+print("Cumulative Variance Ratio:", cumulative_variance_ratio)
+
+variance_first_three = cumulative_variance_ratio[2] * 100
+print(f"The first three components explain approximately {variance_first_three:.2f}% of the variance.")
+
 # We'll use min and max normalized parameters as boundaries
 X_min = np.min(X_normalized, axis=0)
 X_max = np.max(X_normalized, axis=0)
-
 
 # Normalize output data (Y) using StandardScaler
 scaler_Y = StandardScaler()
@@ -102,9 +122,13 @@ plt.show()
 for i, param_name in enumerate(parameter_names):
     plt.figure(figsize=(10, 6))
     for j, result_name in enumerate(result_names):
-        label_text = f"{param_name} vs {result_name}"
-        plt.scatter(X[:, i], Y[:, j], label=label_text)
-        plt.plot(X[:, i], Y_pred[:, j], color='red', label=f"Prediction for {result_name}")
+        sorted_indices = np.argsort(X[:, i])  # Sort data by the X-axis parameter
+        X_sorted = X[sorted_indices, i]
+        Y_sorted = Y[sorted_indices, j]
+        Y_pred_sorted = Y_pred[sorted_indices, j]
+
+        plt.scatter(X_sorted, Y_sorted, label=f"{param_name} vs {result_name}")
+        plt.plot(X_sorted, Y_pred_sorted, color='red', label=f"Prediction for {result_name}")
 
     plt.xlabel(param_name)
     plt.ylabel("Y values")
@@ -113,7 +137,7 @@ for i, param_name in enumerate(parameter_names):
     plt.show()
 
 
-# The function takes input parameters and returns the error between the predicted output and Y_desired
+# The function returns the error between the predicted output and Y_desired
 def objective(X_input):
     # X_input will be in the normalized space, so we need to predict in normalized space first
     X_input_reshaped = X_input.reshape(1, -1)  # Reshape to match the input shape
@@ -130,15 +154,16 @@ def objective(X_input):
 
 initial_guess = np.mean(X_normalized, axis=0)
 
+# None means that parameter can be outside the input data range, predicted based on the regression model
 bounds = [
-    (X_min[0], X_max[0]),   # 'remaining MEK / IP.4'
-    (X_min[1], X_max[1]),   # 'Tmin during concentration'
-    (None, None),           # 'Tstart' (e.g., between 50 and 70)
-    (X_min[3], X_max[3]),   # 'RPM, crystallization'
-    (None, None),           # 'temp slope'
-    (None, None),           # 'start cooling rate slope'
-    (X_min[6], X_max[6]),   # 'middle cooling rate slope'
-    (X_min[7], X_max[7])    # 'final cooling rate slope'
+    (X_min[0], X_max[0]),  # 'remaining MEK / IP.4'
+    (None, None),  # 'Tmin during concentration'
+    (X_min[2], X_max[2]),  # 'Tstart'
+    (X_min[3], X_max[3]),  # 'RPM, crystallization'
+    (None, None),  # 'temp slope'
+    (None, None),  # 'start cooling rate slope'
+    (None, None),  # 'middle cooling rate slope'
+    (None, None)  # 'final cooling rate slope'
 ]
 
 # Perform the optimization to find the best input parameters
